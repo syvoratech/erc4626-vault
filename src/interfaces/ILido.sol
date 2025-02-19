831ff0e99e4ad696cf5a8c5c3b6901ea9cafa9a2
syvoratech <saless@qodeleaf.com>
2025-02-19 19:23:43 +0530

    add: interfaces for weth & lido contracts

--- src/interfaces/ILido.sol
+++ src/interfaces/ILido.sol
@@ -3,4 +3,12 @@ pragma solidity 0.8.28;
 
 interface ILido {
     function submit(address _referral) external payable returns (uint256);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function approve(address user, uint256 amount) external returns (bool);
+
+    function getPooledEthByShares(
+        uint256 _sharesAmount
+    ) external view returns (uint256);
 }
